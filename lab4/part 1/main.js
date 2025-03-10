c5863cdd5a15654929c4a5a5076a8177e7d5d539
MasonC16 <mason.cutts@dcmail.ca>
2025-03-10 14:44:19 -0400

    added variables, replaced values
    added the newStory, xItem, yItem and zItem variables, and added the newStory replaceall things to change the story inside of the results() function.

--- lab4/part 1/main.js
+++ lab4/part 1/main.js
@@ -23,6 +23,16 @@ const insertZ = [spontaneously combusted, melted into a puddle on the sidewalk, 
 randomize.addEventListener('click', result);
 
 function result() {
+	
+	let newStory = storyText;
+
+	const xItem = randomValueFromArray(insertX);
+	const yItem = randomValueFromArray(insertY);
+	const zItem = randomValueFromArray(insertZ);
+	
+	newStory = newStory.replaceAll(':insertx:', xItem);
+	newStory = newStory.replaceAll(':inserty:', yItem);
+	newStory = newStory.replaceAll(':insertz:', zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
@@ -37,4 +47,7 @@ function result() {
 
   story.textContent = ;
   story.style.visibility = 'visible';
+  
 }
+
+
