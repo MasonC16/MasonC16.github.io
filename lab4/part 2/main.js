55e0b9c1f6a3bfee7386c12f7c041da9947ac0db
MasonC16 <mason.cutts@dcmail.ca>
2025-03-24 13:36:17 -0400

    added constants and made for loop
    Added both the fileNames and altText constants, as well as added a for loop to loop through the pictures

--- lab4/part 2/main.js
+++ lab4/part 2/main.js
@@ -10,14 +10,18 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const fileNames = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = {'images/pic1.jpg' : 'picture of eye', 'images/pic2.jpg' : 'picture of a beach', 'images/pic3.jpg' : 'picture of purple and white flowers', 
+'images/pic4.jpg' : 'picture of ancient egyption people', 'images/pic5.jpg' : 'picture of a moth on a leaf' };
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const file in fileNames) {
+	const newImage = document.createElement('img');
+	newImage.setAttribute('src', 'fileNames/${file}');
+	newImage.setAttribute('alt', 'altText[file]');
+	thumbBar.appendChild(newImage);	
+}
 
 /* Wiring up the Darken/Lighten button */
