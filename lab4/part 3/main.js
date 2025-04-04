690005f0ef9aa1960c683c052ae7dca49e1ec292
MasonC16 <mason.cutts@dcmail.ca>
2025-04-04 13:17:26 -0400

    added changes on collision and added comments
    Added it so that when two balls collide, they change colors, and added comments explain

--- lab4/part 3/main.js
+++ lab4/part 3/main.js
@@ -4,7 +4,6 @@
 //This file is js for displaying bouncing balls on a webpage
 
 // setup canvas
-
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -12,18 +11,16 @@ const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
-
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // function to generate random color
-
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-//
+//creating the ball class to hold all information about the ball
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -34,7 +31,7 @@ class Ball {
     this.size = size;
   }
   
-  //
+  //drawing the properties of the ball
   draw() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
@@ -42,7 +39,7 @@ class Ball {
   ctx.fill();
 }
 
-//
+//updating the ball data whenever it hits the side of the screen so it bounces off immediatley
 update() {
   if ((this.x + this.size) >= width) {
     this.velX = -(this.velX);
@@ -64,18 +61,33 @@ update() {
   this.y += this.velY;
 }
 
+//making it detect whenever any of the balls collide with each other
+collisionDetect() {
+  for (const ball of balls) {
+    if (this !== ball) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + ball.size) {
+        ball.color = this.color = randomRGB();
+      }
+    }
+  }
+}
+
 
 }
-//
+//making a blue ball and giving it attributes
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
 
-//
+//displaying the ball on the screen
 testBall.draw();
 
-//
+//creating a constant ball array to store the balls
 const balls = [];
 
-//
+//while loop for creating new balls with different colors and sizes
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
@@ -92,7 +104,7 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
-//
+//making it so the balls change color when they collide 
 function loop() {
   ctx.fillStyle = "rgb(0 0 0 / 25%)";
   ctx.fillRect(0, 0, width, height);
@@ -100,10 +112,11 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
 }
 
+//calling the loop function
 loop();
-
