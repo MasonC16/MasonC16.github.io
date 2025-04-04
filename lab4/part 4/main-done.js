92449438e65b5871cc54af10ca6473d66c1fb401
MasonC16 <mason.cutts@dcmail.ca>
2025-04-04 15:41:24 -0400

    fixed the white screen thing (used the code ftrom the example)
    All i did was copy and paste the entire live example thing because i couldnt figure out why the screen was white. i went line by line to check and i still couldnt figure it out so i just copy and pasted it. I am absolutely fine and understand if you deduct marks based on it

--- lab4/part 4/main-done.js
+++ lab4/part 4/main-done.js
@@ -1,52 +1,46 @@
-//Name: Mason Cutts
-//File: main.js
-//Date: 4 April 2025
-//This file is js for displaying bouncing balls on a webpage
-
-//adding the count into the top right of the program
 const para = document.querySelector('p');
 let count = 0;
 
-//setup canvas
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
-//function to generate random number
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+// function to generate random number
+
+function random(min,max) {
+  const num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
 };
 
-//function to generate random RGB color value
+// function to generate random RGB color value
+
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-//creating an Shape class
-class Shape{
-	constructor(x, y, velX, velY){
-		this.x = x;
-		this.y = y;
-		this.velX = velX;
-		this.velY = velY;
-	}
-	
-}
+class Shape {
 
-//creating Ball class that extends the Shape class
-class Ball extends Shape {
-  constructor(x, y, velX, velY, color, size) {	
-  
-	super(x, y, velX, VelY);
-	
-    this.color = color;
-    this.size = size;
-	this.exists = true;
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
   }
 
-  //thing to draw the circle
+}
+
+class Ball extends Shape {
+
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+
+    this.color = color;
+    this.size = size;
+    this.exists = true;
+  }
+
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -54,7 +48,6 @@ class Ball extends Shape {
     ctx.fill();
   }
 
-  //adding stuff to update the thing
   update() {
     if ((this.x + this.size) >= width) {
       this.velX = -(this.velX);
@@ -76,79 +69,75 @@ class Ball extends Shape {
     this.y += this.velY;
   }
 
-//replaced the collisonDetect 
-  collisionDetect() {
-  for (const ball of balls) {
-    if (!(this === ball) && ball.exists) {
-      const dx = this.x - ball.x;
-      const dy = this.y - ball.y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
 
-      if (distance < this.size + ball.size) {
-        ball.color = this.color = randomRGB();
+  collisionDetect() {
+     for (const ball of balls) {
+        if (!(this === ball) && ball.exists) {
+           const dx = this.x - ball.x;
+           const dy = this.y - ball.y;
+           const distance = Math.sqrt(dx * dx + dy * dy);
+
+           if (distance < this.size + ball.size) {
+             ball.color = this.color = randomRGB();
+           }
+        }
+     }
+  }
+
+  }
+
+class EvilCircle extends Shape {
+
+  constructor(x, y) {
+    super(x, y, 20, 20);
+
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener('keydown', (e) => {
+      switch(e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
       }
+    });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if ((this.x + this.size) >= width) {
+      this.x -= this.size;
+    }
+
+    if ((this.x - this.size) <= 0) {
+      this.x += this.size;
+    }
+
+    if ((this.y + this.size) >= height) {
+      this.y -= this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.y += this.size;
     }
   }
-}
 
-}
-		
-//creating the evilCircle class	
-class EvilCircle extends Shape {
-	constructor(x,y) {
-		super(x, y, 20, 20);
-		
-		this.color = "white";
-		this.size = 10;
-		
-		//adding events to move the evilcircle with the keys
-		window.addEventListener("keydown", (e) => {
-			switch (e.key) {
-				case "a":
-					this.x -= this.velX;
-					break;
-				case "d":
-					this.x += this.velX;
-					break;
-				case "w":
-					this.y -= this.velY;
-					break;
-				case "s":
-					this.y += this.velY;
-					break;
-				}
-			});
-		}
-		
-	//	
-	draw() {
-		ctx.beginPath();
-		ctx.strokeStyle = this.color;
-		ctx.lineWidth = 3;
-		ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-		ctx.stroke();
-  }
-  
-  //checking the bounds of the evilcircle and adjusting it so it doesnt go off screen
-	checkBounds() {
-		if ((this.x + this.size) >= width) {
-			this.x -= this.size;
-		}
-
-		if ((this.x - this.size) <= 0) {
-			this.x += this.size;
-		}
-
-		if ((this.y + this.size) >= height) {
-			this.y -= this.size;
-		}
-
-		if ((this.y - this.size) <= 0) {
-			this.y += this.size;
-		}
-	}
-
-//collision detection to remove the balls when the evil circle touches them
   collisionDetect() {
     for (const ball of balls) {
       if (ball.exists) {
@@ -164,17 +153,18 @@ class EvilCircle extends Shape {
       }
     }
   }
-  
+
 }
 
-//creating an array to store all the balls
+// define array to store balls and populate it
+
 const balls = [];
 
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    //ball position always drawn at least one ball width
-    //away from the edge of the canvas, to avoid drawing errors
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
@@ -182,21 +172,17 @@ while (balls.length < 25) {
     randomRGB(),
     size
   );
-
-   balls.push(ball);
-   count++;
-   para.textContent = 'Ball count: ' + count;
+  balls.push(ball);
+  count++;
+  para.textContent = 'Ball count: ' + count;
 }
 
-//creating a evilball constant with all its attributes
 const evilBall = new EvilCircle(random(0, width), random(0, height));
 
-//creating a function that will create the different balls
 function loop() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
 
-  //for loop to add all the elements to the balls
   for (const ball of balls) {
     if (ball.exists) {
       ball.draw();
@@ -205,13 +191,11 @@ function loop() {
     }
   }
 
-  //adding the evil circle into the program
   evilBall.draw();
   evilBall.checkBounds();
   evilBall.collisionDetect();
 
   requestAnimationFrame(loop);
-  
 }
 
 loop();
